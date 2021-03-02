58191f4fdbb077e1afacf743cb07bef30e7960e9
Syed Nazmus Shakib <sakibsyed001@outlook.com>
2021-03-02 12:25:35 +0600

    authentication

---
+++ controllers/authController.js
@@ -0,0 +1,199 @@
+const User = require('./../models/userModel');
+const { promisify } = require('util');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const catchAsync = require('../utils/catchAsync');
+const sendEmail = require('./../utils/email');
+const AppError = require('../utils/appError');
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  createSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //1)  Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  //2)  Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  //3)  If everything ok, send token to client
+  createSendToken(user, 200, res);
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //  1)Get token from req.header & check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in. Please login to get access.', 401)
+    );
+  }
+  //  2)Verify Token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  //  3)Check if user still exists
+  let currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('User belonging to this token does no longer exist.', 401)
+    );
+  }
+  //  4)Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError(
+        'User recently changed password! Please log in to continue.',
+        401
+      )
+    );
+  }
+
+  req.user = currentUser;
+  next();
+});
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError(
+          'You do not have the permission to perform this task.',
+          403
+        )
+      );
+    }
+    next();
+  };
+};
+
+exports.forgetPassword = catchAsync(async (req, res, next) => {
+  //  1)Get user from DB based on req.body
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError(`User with this email does not exist`, 404));
+  }
+  //  2)Generate random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  //  3)Send it to user's mail
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetpassword/${resetToken}`;
+  const message = `Forgot your password? \n \n Sumit a PATCH request with your new password and passwordConfirm to:${resetURL}.\n \n  If you didn't forget your password, please ignore this email`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 mins)',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'succes',
+      message: 'Token send to email',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError(
+        `There was an error sending the mail. please try again letter!`,
+        500
+      )
+    );
+  }
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  //  1)Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  //  2)If the token has not been expired, and there is user, send the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  //  3)Update changed password at the property of user
+  //  In userModel (~65)
+
+  //  4)Log the user in, send JWT
+  createSendToken(user, 200, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  //  1)Get user from collection
+  const user = await await User.findById(req.user.id);
+
+  //  2)Check if posted current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError(`Your current password is wrong`, 401));
+  }
+
+  //  3)If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  //  4)Log uuser in,send JWT
+  createSendToken(user, 200, res);
+});
--- server.js
+++ server.js
@@ -2,12 +2,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const colors = require('colors');
 
-process.on('uncaughtException', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNCAUGHT EXCEPTION! SHUTTING DOWN....');
-  process.exit(1);
-});
-
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -48,6 +42,3 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
-
-console.log(x);
-//test
