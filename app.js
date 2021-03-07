7aeb0a2093715c1f3b1bf39b2d76e1b2612324ab
Syed Nazmus Shakib <sakibsyed001@outlook.com>
2021-03-07 17:58:03 +0600

    almost

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -51,6 +52,7 @@ if (process.env.NODE_ENV === 'development') {
 //------------
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 //3 ) ERROR HANDELING:-
 //------------
